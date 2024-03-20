b27795d82ca0e14c0f2f5e8a4e92f802656b3463
Mikail Üstüner <163590351+0x1771@users.noreply.github.com>
2024-03-21 02:52:33 +0300

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,218 @@
+import 'package:flutter/material.dart';
+import 'package:fluttericon/entypo_icons.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:fluttericon/font_awesome5_icons.dart';
+import 'package:fluttericon/font_awesome_icons.dart';
+import 'package:animated_text_kit/animated_text_kit.dart';
+
+void main() {
+  runApp(
+    MyApp(),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(fontFamily: 'Monoton'),
+      home: Scaffold(
+        backgroundColor: Color.fromARGB(255, 0, 0, 0),
+        body: SafeArea(
+          child: Center(
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: <Widget>[
+                CircleAvatar(
+                  backgroundImage: AssetImage('images/me.png'),
+                  radius: 70.0,
+                ),
+                SizedBox(
+                  height: 10.0,
+                ),
+                Text(
+                  'Accounts',
+                  style: TextStyle(fontSize: 30, color: Colors.white),
+                ),
+                SizedBox(
+                  height: 20.0,
+                ),
+                Text(
+                  '',
+                  style: GoogleFonts.audiowide(
+                    fontSize: 16,
+                    color: Colors.white,
+                  ),
+                ),
+                Container(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: <Widget>[
+                      ShaderMask(
+                        blendMode: BlendMode.srcATop,
+                        shaderCallback: (bounds) => LinearGradient(
+                          begin: Alignment.topLeft,
+                          end: Alignment.bottomRight,
+                          colors: [
+                            Color.fromARGB(255, 255, 255, 255),
+                            Color.fromARGB(139, 42, 222, 235),
+                            Color.fromARGB(255, 0, 174, 255)
+                          ],
+                        ).createShader(bounds),
+                        child: Icon(
+                          FontAwesome.github_circled,
+                          color: Colors.white,
+                        ),
+                      ),
+                      SizedBox(
+                        width: 10.0,
+                      ),
+                      Text(
+                        '@0x1772',
+                        style: GoogleFonts.audiowide(
+                          fontSize: 16,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Container(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: <Widget>[
+                      ShaderMask(
+                          blendMode: BlendMode.srcATop,
+                          shaderCallback: (bounds) => LinearGradient(
+                                begin: Alignment.topLeft,
+                                end: Alignment.bottomRight,
+                                colors: [
+                                  const Color(0xff6f3ac0),
+                                  const Color(0xffeb2a7b),
+                                  const Color(0xfff48f54)
+                                ],
+                              ).createShader(bounds),
+                          child: Icon(FontAwesome5.instagram,
+                              color: Colors.white)),
+                      SizedBox(
+                        height: 70.0,
+                        width: 10.0,
+                      ),
+                      Text(
+                        '@0x1772',
+                        style: GoogleFonts.audiowide(
+                          fontSize: 16,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Container(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: <Widget>[
+                      ShaderMask(
+                        blendMode: BlendMode.srcATop,
+                        shaderCallback: (bounds) => LinearGradient(
+                          begin: Alignment.topLeft,
+                          end: Alignment.bottomRight,
+                          colors: [
+                            Color.fromARGB(255, 255, 255, 255),
+                            Color.fromARGB(255, 31, 145, 238)
+                          ],
+                        ).createShader(bounds),
+                        child: Icon(
+                          FontAwesome5.safari,
+                          color: Colors.white,
+                        ),
+                      ),
+                      SizedBox(
+                        width: 10.0,
+                      ),
+                      Text(
+                        'SmileOS.live',
+                        style: GoogleFonts.audiowide(
+                          fontSize: 16,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Container(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: <Widget>[
+                      ShaderMask(
+                        blendMode: BlendMode.srcATop,
+                        shaderCallback: (bounds) => LinearGradient(
+                          begin: Alignment.topLeft,
+                          end: Alignment.bottomRight,
+                          colors: [
+                            Color.fromARGB(255, 25, 0, 255),
+                            Color.fromARGB(255, 49, 147, 228)
+                          ],
+                        ).createShader(bounds),
+                        child: Icon(
+                          FontAwesome5.telegram_plane,
+                          color: Colors.white,
+                        ),
+                      ),
+                      SizedBox(
+                        height: 70.0,
+                        width: 10.0,
+                      ),
+                      Text(
+                        '@FakeSmileUX',
+                        style: GoogleFonts.audiowide(
+                          fontSize: 16,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Container(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: <Widget>[
+                      ShaderMask(
+                        blendMode: BlendMode.srcATop,
+                        shaderCallback: (bounds) => LinearGradient(
+                          begin: Alignment.topLeft,
+                          end: Alignment.bottomRight,
+                          colors: [
+                            Color.fromARGB(255, 17, 221, 160),
+                            Color.fromARGB(255, 67, 161, 238)
+                          ],
+                        ).createShader(bounds),
+                        child: Icon(
+                          FontAwesome5.linkedin,
+                          color: Colors.white,
+                        ),
+                      ),
+                      SizedBox(
+                        width: 10.0,
+                      ),
+                      Text(
+                        'Loading...',
+                        style: GoogleFonts.audiowide(
+                            fontSize: 16, color: Colors.white),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
